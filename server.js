2ae737ec7c7b516c9be13d370d889b771b522ab0
PRASA8591 <punsaraprasanna@gmail.com>
2026-06-11 15:16:22 +0530

    Remove CORS settings from server.js
    Removed CORS configuration for specific origins.

--- server.js
+++ server.js
@@ -322,8 +322,3 @@ setInterval(async () => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use(cors({
-  origin: ["https://inv.prasatek.site", "https://frontend-zeta-three-86.vercel.app"],
-  credentials: true 
-}));
